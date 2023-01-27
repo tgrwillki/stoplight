c33622b19ca3a7c718a937536ab801c96a599d78
wil.kirwan@gmail.com <wil.kirwan@gmail.com>
2023-01-27 15:28:37 -0600

    Modified 17 files
    Committed by wil-kirwan on behalf of wil.kirwan@gmail.com

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE3Njk5OQ/spectral.js?branch_slug=main&token=c6dce4c0-c59f-4e1c-ad6a-a060082e16a1";
+export default { extends: ruleset };
